ff82a2509b643bc02e766d7f4551536875242c47
serranuraran <90554@DESKTOP-K3RAL3M>
2021-04-26 01:44:08 +0300

    commit2

---
+++ loopdemo/src/loopdemo/Main.java
@@ -0,0 +1,23 @@
+package loopdemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+ 		//While
+		int i = 2;
+		while (i < 10) {
+			System.out.println(i);
+			i += 2;
+		}
+
+		System.out.println("While döngüsü bitti");
+		
+		//Do-While
+		int j=1;
+		do {
+			System.out.println(j);
+			j += 2;
+		}while (j < 10);
+		System.out.println("Do-While döngüsü bitti");
+	}
+}
